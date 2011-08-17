bbda49bb54e0ad42f50406effe105b8dc6eac5c5
Carlos Zúñiga <carlos.zun@gmail.com>
2011-08-17 09:47:12 -0500

    añadiendo opciones de ejecución

--- retwitter.py
+++ retwitter.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+import sys
 import time
 import tweepy
+from optparse import OptionParser
 
 from config import consumer_key, consumer_secret, access_token_secret, access_token_key
 
@@ -8,7 +10,7 @@ username = 'AQPGLUG'
 listname = 'miembros'
 
 def findaqpglug(timeline):
-    for tweet in timeline:
+	for tweet in timeline:
         if tweet.text.lower().find('aqpglug') != -1:
             yield tweet
 
@@ -18,7 +20,7 @@ def get_auth_api():
     api = tweepy.API(auth)
     return api, auth
 
-def main():
+def retweet():
     api, auth = get_auth_api()
     user = api.get_user(auth.username)
     members_list = api.get_list(user.screen_name, listname)
@@ -34,6 +36,30 @@ def main():
         #time.sleep(60)
         time.sleep(2)
 
+def verify(auth):
+    # 1285931
+    redirect_url = auth.get_authorization_url()
+    print "go to this url %s" % redirect_url
+    verify_pin = raw_input('Type the pin: ')
+    auth.set_access_token(verify_pin)
+    # save auth.access_token.key
+    # and  auth.access_token.secret
+
+def main(args=[]):
+    parser = OptionParser()
+    parser.add_option("-r", "--register", action="store_false", default=False
+                      help="Register your twitter account")
+    parser.add_option("-a", "--account", type="string",
+                      help="Run the bot for that account")
+    parser.add_option("-l", "--list", type="string",
+                      help="Retweet from this list")
+    parser.add_option("-k", "--keyword", type="string",
+                      help="Keyword that triggers the retweet")
+    options, args = parser.parse_args()
+
+    if options.register:
+        verify()
+
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
 
