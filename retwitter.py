d257156fd399113b20ff36cae1e573b88e83fb9a
Carlos Zúñiga <carlos.zun@gmail.com>
2011-08-17 13:14:59 -0500

    lots of changes

--- retwitter.py
+++ retwitter.py
@@ -1,65 +1,130 @@
 #!/usr/bin/env python
+import os
 import sys
 import time
 import tweepy
 from optparse import OptionParser
+from configobj import ConfigObj
 
-from config import consumer_key, consumer_secret, access_token_secret, access_token_key
+class App(object):
+    def __init__(self):
+        self.config_file = os.path.expanduser('~/.retweeter.ini')
+        self.config = ConfigObj(self.config_file)
 
-username = 'AQPGLUG'
-listname = 'miembros'
+    def load_auth(self):
+        try:
+            self.c_key = self.config['consumer_key']
+            self.c_secret = self.config['consumer_secret']
+            self.auth = tweepy.OAuthHandler(self.c_key, self.c_secret)
 
-def findaqpglug(timeline):
-	for tweet in timeline:
-        if tweet.text.lower().find('aqpglug') != -1:
-            yield tweet
+        except KeyError, e:
+            print "You need to configure both the consumer key and secret"
+            print "Try the --config-key and --config-secret options"
+            sys.exit(1)
 
-def get_auth_api():
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token_key, access_token_secret)
-    api = tweepy.API(auth)
-    return api, auth
+        except tweepy.TweepError, e:
+            print "Authorization error, check that your app's key and secret are valid"
+            sys.exit(1)
 
-def retweet():
-    api, auth = get_auth_api()
-    user = api.get_user(auth.username)
-    members_list = api.get_list(user.screen_name, listname)
-    retweets = []
-    for tweet in members_list.timeline():
-        retweets.append(tweet)
-        #if tweet.text.lower().find('aqpglug') != -1:
-        #    retweets.append(tweet)
+    def load_user(self, account):
+        try:
+            a_key = self.config[account]['access_token_key']
+            a_secret = self.config[accout]['access_token_secret']
+            self.auth.set_access_token(a_key, a_secret)
 
-    for tweet in retweets[:5]:
-        print tweet.text
-        #tweepy.retweet(tweet.id)
-        #time.sleep(60)
-        time.sleep(2)
+        except KeyError, e:
+            if e.args[0] == 'account':
+                print "User account not registered"
+            else:
+                print "Access token invalid, please register account again"
+            sys.exit(1)
 
-def verify(auth):
-    # 1285931
-    redirect_url = auth.get_authorization_url()
-    print "go to this url %s" % redirect_url
-    verify_pin = raw_input('Type the pin: ')
-    auth.set_access_token(verify_pin)
-    # save auth.access_token.key
-    # and  auth.access_token.secret
+        except tweepy.TweepError, e:
+            print "Authorization error, check that your app's key and secret are valid"
+            sys.exit(1)
 
-def main(args=[]):
-    parser = OptionParser()
-    parser.add_option("-r", "--register", action="store_false", default=False
-                      help="Register your twitter account")
-    parser.add_option("-a", "--account", type="string",
-                      help="Run the bot for that account")
-    parser.add_option("-l", "--list", type="string",
-                      help="Retweet from this list")
-    parser.add_option("-k", "--keyword", type="string",
-                      help="Keyword that triggers the retweet")
-    options, args = parser.parse_args()
+    def get_list_timeline(self, listname):
+        twitter_list = api.get_list(auth.username, listname)
+        return twitter_list.timeline()
 
-    if options.register:
-        verify()
+    def run(self, args):
+        parser = OptionParser()
+        parser.add_option("-r", "--register", action="store_true", default=False,
+                          help="Register your twitter account")
+        parser.add_option("-a", "--account", type="string",
+                          help="Run the bot for that account")
+        parser.add_option("-l", "--list", type="string",
+                          help="Retweet from this list")
+        parser.add_option("-k", "--keyword", type="string",
+                          help="Keyword that triggers the retweet")
+        parser.add_option("--config-key", type="string",
+                          help="Configure application's consumer key")
+        parser.add_option("--config-secret", type="string",
+                          help="Configure application's consumer secret")
+
+        options, args = parser.parse_args()
+
+        if options.config_key and options.config_secret:
+            self.config_app(options.config_key, options.config_secret)
+            self.config.write()
+            self.load_auth()
+            return 0
+
+        if options.register:
+            self.load_auth()
+            self.register()
+            self.config.write()
+            return 0
+
+        if options.account and options.list and options.keyword:
+            self.load_user(options.account)
+            self.work(account, listname, keyword)
+            self.config.write()
+        return 0
+            
+    def work(self, account, listname, keyword):
+
+        try:
+            last_id = self.config[account][listname]
+        except KeyError, e:
+            last_id = None
+
+        self.api = tweepy.API(self.auth)
+        tweets = self.get_list_timeline(options.list)
+        retweets = []
+        for tweet in tweets:
+            if tweet.text.lower().find(options.keyword) != -1:
+                retweets.append(tweet)
+
+
+        for tweet in reversed(retweets[:5]):
+            tweepy.retweet(tweet.id)
+            time.sleep(60)
+        
+        last_id = tweets[0].id
+        self.config[account][listname] = last_id
+
+
+    def register(self):
+        # 1285931
+        redirect_url = self.auth.get_authorization_url()
+        print "Go to this url: %s" % redirect_url
+        print "and grant access, it will return an access number"
+
+        # TODO: do this in a loop to avoid empty pin
+        verify_pin = raw_input('Type the pin: ')
+        self.auth.get_access_token(verify_pin)
+        username = self.auth.get_username()
+        # TODO: check if username exist and ask if it's ok to overwrite
+        self.config[username] = {}
+        self.config[username]['key'] = self.auth.access_token.key
+        self.config[username]['secret'] = self.auth.access_token.secret
+
+    def config_app(self, consumer_key, consumer_secret):
+        self.config['consumer_key'] = consumer_key
+        self.config['consumer_secret'] = consumer_secret
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    app = App()
+    app.run(sys.argv[1:])
 
