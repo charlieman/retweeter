98890158495dfe1f2089aabfff86e4cea4c85370
Carlos Zúñiga <carlos.zun@gmail.com>
2011-08-17 14:22:23 -0500

    turn some functions into lazy loading properties

--- retwitter.py
+++ retwitter.py
@@ -11,28 +11,48 @@ class App(object):
         self.config_file = os.path.expanduser('~/.retweeter.ini')
         self.config = ConfigObj(self.config_file)
 
-    def load_auth(self):
+    _auth=None
+    @property
+    def auth(self):
+        if self._auth:
+            return self._auth
         try:
             self.c_key = self.config['consumer_key']
             self.c_secret = self.config['consumer_secret']
-            self.auth = tweepy.OAuthHandler(self.c_key, self.c_secret)
+            self._auth = tweepy.OAuthHandler(self.c_key, self.c_secret)
 
         except KeyError, e:
+            if self.debug:
+                print e
             print "You need to configure both the consumer key and secret"
             print "Try the --config-key and --config-secret options"
             sys.exit(1)
 
         except tweepy.TweepError, e:
+            if self.debug:
+                print e
             print "Authorization error, check that your app's key and secret are valid"
             sys.exit(1)
+        return self._auth
+
+    _api=None
+    @property
+    def api(self):
+        if self._api:
+            return self._api
+        self._api = tweepy.API(self.auth)
+        return self._api
 
     def load_user(self, account):
+        import pdb;pdb.set_trace()
         try:
-            a_key = self.config[account]['access_token_key']
-            a_secret = self.config[accout]['access_token_secret']
+            a_key = self.config[account]['key']
+            a_secret = self.config[account]['secret']
             self.auth.set_access_token(a_key, a_secret)
 
         except KeyError, e:
+            if self.debug:
+                print e
             if e.args[0] == 'account':
                 print "User account not registered"
             else:
@@ -40,12 +60,14 @@ class App(object):
             sys.exit(1)
 
         except tweepy.TweepError, e:
+            if self.debug:
+                print e.msg
             print "Authorization error, check that your app's key and secret are valid"
             sys.exit(1)
 
-    def get_list_timeline(self, listname):
-        twitter_list = api.get_list(auth.username, listname)
-        return twitter_list.timeline()
+    def get_list_timeline(self, listname, since_id):
+        twitter_list = self.api.get_list(self.auth.username, listname)
+        return twitter_list.timeline(since_id=since_id)
 
     def run(self, args):
         parser = OptionParser()
@@ -61,24 +83,26 @@ class App(object):
                           help="Configure application's consumer key")
         parser.add_option("--config-secret", type="string",
                           help="Configure application's consumer secret")
+        parser.add_option("-d", "--debug", action="store_true", default=False,
+                          help="Debug")
 
         options, args = parser.parse_args()
+
+        self.debug = options.debug
 
         if options.config_key and options.config_secret:
             self.config_app(options.config_key, options.config_secret)
             self.config.write()
-            self.load_auth()
             return 0
 
         if options.register:
-            self.load_auth()
             self.register()
             self.config.write()
             return 0
 
         if options.account and options.list and options.keyword:
-            self.load_user(options.account)
-            self.work(account, listname, keyword)
+            self.load_user(options.account.lower())
+            self.work(options.account, options.list, options.keyword)
             self.config.write()
         return 0
             
@@ -87,17 +111,20 @@ class App(object):
         try:
             last_id = self.config[account][listname]
         except KeyError, e:
+            if e.args[0] == account:
+                self.config[account] = {}
             last_id = None
 
-        self.api = tweepy.API(self.auth)
-        tweets = self.get_list_timeline(options.list)
+        tweets = self.get_list_timeline(listname, last_id)
         retweets = []
         for tweet in tweets:
-            if tweet.text.lower().find(options.keyword) != -1:
+            if tweet.text.lower().find(keyword.lower()) != -1:
                 retweets.append(tweet)
 
-
         for tweet in reversed(retweets[:5]):
+            if self.debug:
+                print tweet.text
+                continue
             tweepy.retweet(tweet.id)
             time.sleep(60)
         
@@ -114,7 +141,7 @@ class App(object):
         # TODO: do this in a loop to avoid empty pin
         verify_pin = raw_input('Type the pin: ')
         self.auth.get_access_token(verify_pin)
-        username = self.auth.get_username()
+        username = self.auth.get_username().lower()
         # TODO: check if username exist and ask if it's ok to overwrite
         self.config[username] = {}
         self.config[username]['key'] = self.auth.access_token.key
